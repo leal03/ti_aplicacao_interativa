a3c1055ea33e1fd68a285ab27245b4e4b3593ccf
Pedrosl16 <p-lucas2009@hotmail.com>
2024-06-09 22:25:12 -0300

    Adicionando autenticação na tela de login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,25 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    // Log para verificar os dados recebidos
+    console.log('Tentativa de login:', { email, password });
+
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
-      return res.status(401).json({ message: 'Email ou senha incorretos.' });
+      return res.status(401).json({ error: 'Usuário não encontrado' });
     }
 
-    const validPassword = await user.validPassword(password);
-
-    if (!validPassword) {
-      return res.status(401).json({ message: 'Email ou senha incorretos.' });
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Senha incorreta' });
     }
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.status(200).json({ token });
   } catch (error) {
     res.status(400).json({ error: error.message });
